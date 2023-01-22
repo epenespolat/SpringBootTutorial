98c90659076aade442e2484a2a212381f585c1f1
Enes Polat <epenespolat@gmail.com>
2023-01-22 22:55:29 +0300

    @Bean revised

--- src/main/java/com/enespolat/controller/BeanController.java
+++ src/main/java/com/enespolat/controller/BeanController.java
@@ -22,21 +22,4 @@ public class BeanController {
         return beanConfig.beanDto()+ "";
     }
 
-    @Autowired
-    Logger LOG;
-
-    /*public BeanController() {
-        LOG.info("Log info çağırıldı");
-    }*/
-    @PostConstruct
-    public void init(){
-        LOG.info("Log info çağırıldı");
-    }
-
-    public static void main(String[] args) {
-        BeanController beanController= new BeanController();
-        System.out.println(beanController);
-    }
-
-
 }
