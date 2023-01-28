aa00133927eaf418e7096a022bafdbbf8f6e0dad
Enes Polat <epenespolat@gmail.com>
2023-01-28 19:33:39 +0300

    messages.properties

--- src/main/java/com/enespolat/controller/ThymeLeafController.java
+++ src/main/java/com/enespolat/controller/ThymeLeafController.java
@@ -30,4 +30,12 @@ public class ThymeLeafController {
         model.addAttribute("key_model2","Ben Modelden geldim-2.");
         return "thymeleaf_file/thymeleaf3";
     }
+
+    // http://localhost:8080/thymeleaf4
+    @GetMapping("/thymeleaf4")
+    public String getThymeleaf4Model(Model model){
+        model.addAttribute("key_model1","Ben Modelden geldim-4.");
+
+        return "thymeleaf4";
+    }
 }
