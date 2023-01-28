20ff9927c4ec39dd3c1f0335531ed8db696474fb
Enes Polat <epenespolat@gmail.com>
2023-01-28 19:23:00 +0300

    thymeleaf css,html,model

--- src/main/java/com/enespolat/controller/ThymeLeafController.java
+++ src/main/java/com/enespolat/controller/ThymeLeafController.java
@@ -1,6 +1,8 @@
 package com.enespolat.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +13,21 @@ public class ThymeLeafController {
     //@ResponseBody
     public String getThymeleaf1(){
         return "thymeleaf1";
+    }
+
+    // http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model(Model model){
+        model.addAttribute("key_model1","Ben Modelden geldim-1.");
+        model.addAttribute("key_model2","Ben Modelden geldim-2.");
+        return "thymeleaf1";
+    }
+
+    // http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf3Model(Model model){
+        model.addAttribute("key_model1","Ben Modelden geldim-1.");
+        model.addAttribute("key_model2","Ben Modelden geldim-2.");
+        return "thymeleaf_file/thymeleaf3";
     }
 }
