c0b75c485ef62c467f6ed6d69039b57ed5e116e0
Enes Polat <epenespolat@gmail.com>
2023-01-29 02:06:25 +0300

    EmployeeDto

---
+++ src/main/java/com/enespolat/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package com.enespolat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+}
