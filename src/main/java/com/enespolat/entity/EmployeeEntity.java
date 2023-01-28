b80a1f228390ec5222815bdd0d240855e924f064
Enes Polat <epenespolat@gmail.com>
2023-01-29 02:19:49 +0300

    BaseEntity (@MappedSuperclass, EntityListeners,@CreatedBy,@CreatedDate,@LastModifiedBy,@LastModifiedDate,@CreationTimestamp,@Temporal) EmployeeEntity (@Entity,@Table, @Id, @Column, @GeneratedValue , )

---
+++ src/main/java/com/enespolat/entity/EmployeeEntity.java
@@ -0,0 +1,34 @@
+package com.enespolat.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+@Data
+@NoArgsConstructor
+@Log4j2
+@Builder
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_name")
+    private String emailId;
+
+    public EmployeeEntity(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+}
