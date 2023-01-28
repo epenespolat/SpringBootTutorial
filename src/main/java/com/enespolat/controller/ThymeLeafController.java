783ceedb7b402dd7f748ab41eee17dc00f1f0c13
Enes Polat <epenespolat@gmail.com>
2023-01-28 20:26:42 +0300

    @Pathvariable

--- src/main/java/com/enespolat/controller/ThymeLeafController.java
+++ src/main/java/com/enespolat/controller/ThymeLeafController.java
@@ -5,6 +5,7 @@ import com.enespolat.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -12,6 +13,14 @@ import java.util.List;
 
 @Controller
 public class ThymeLeafController {
+
+    // Optional
+    // http://localhost:8080
+    @GetMapping({"/","index"})
+    public String index(){return "index";};
+
+
+
     // http://localhost:8080/thymeleaf1
     @GetMapping("/thymeleaf1")
     //@ResponseBody
@@ -72,6 +81,20 @@ public class ThymeLeafController {
         liste.add(ProductDto.builder().productId(3L).productName("Urun Adi4").productPrice(5500).build());
         model.addAttribute("product_list", liste);
         return "thymeleaf6";
+    }
+
+
+    // http://localhost:8080/thymeleaf7/
+    @GetMapping({"/thymeleaf7","/thymeleaf7/","/thymeleaf7/{id}"})
+    public String getThymeleaf7ModelObject(Model model, @PathVariable(name ="id", required = false) Long id) {
+        if (id!=null){
+            model.addAttribute("key_model1", "id: " +id);
+        }else {
+            model.addAttribute("key_model1", "id bulunamadı");
+        }
+
+
+        return "thymeleaf7";
     }
 
 }
